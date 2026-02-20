030b64e95f3cd46131d01d6cf12066c7e4873d4c
hai.hoang@hangchinhhieu.vn <77879961+haihoanghch@users.noreply.github.com>
2026-02-21 05:17:24 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+<script type="module">
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+export const supabase = createClient(
+"https://wvfcnozpxgekyovylwjw.supabase.co",
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2ZmNub3pweGdla3lvdnlsd2p3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MjkzODQsImV4cCI6MjA4NzAwNTM4NH0.oDOznylKm_I1-RwHx7Xqk2LCy_tIbZzjgwhCEPeDhNw"
+);
+
+</script>
